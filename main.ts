cbf6897e1c8b8dec99600e1eaf768e0d5ac08e23
ioedeveloper <ioedeveloper@DESKTOP-113GA6A>
2018-05-07 22:00:26 +0100

    Initial Commit

--- main.ts
+++ main.ts
@@ -1,30 +1,30 @@
-import express = require('express');
-import bodyParser = require('body-parser');
-import path = require('path');
-import * as home from './controllers/home';
-import { createConnection } from 'typeorm';
+import express = require("express");
+import bodyParser = require("body-parser");
+import path = require("path");
+import * as home from "./controllers/home";
+import { createConnection } from "typeorm";
 import * as leavectrl from "./controllers/leaveController";
-//create express server
+// create express server
 
 
-//create app db connection.
+// create app db connection.
  createConnection().then(async connection => {
     const app = express();
     console.log("DB online!");
-    const approot = './';
+    const approot = "./";
     const appport = process.env.Port || 8001;
-    //setup express for json parsing even with urlencoding
+    // setup express for json parsing even with urlencoding
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(express.static(path.join(approot,'dist')));
+    app.use(express.static(path.join(approot,"dist")));
 
-    //serve and respond to routes by api
-    app.get('/home', home.home);
-    app.get('/login',home.login);
+    // serve and respond to routes by api
+    app.get("/home", home.home);
+    app.get("/login",home.login);
 
-    //routes for leave
-    app.get('/api/leaves', leavectrl.index);
-    //default fall through
+    // routes for leave
+    app.get("/api/leaves", leavectrl.index);
+    // default fall through
     // app.get('*', (req: Request, res: Response)=>{
     //     res.sendFile(approot,'dist/index.html');
     // });
